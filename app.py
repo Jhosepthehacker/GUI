be9dc4c89ec652c85c5a82ed4a032b2efeb0be7a
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-07-04 19:58:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import Menu
+from sys import exit
+from time import sleep
 
 def salir_pregunta():
     mensaje_de_pregunta = messagebox.askquestion(
@@ -18,21 +20,29 @@ def saludar_al_usuario():
   message="Hola, Saludos"       
 )
 
-ventana = tk.Tk()
-ventana.title("Proyecto GUI")
-ventana.geometry("400x400")
+preguntar_al_usuario = input("Este programa requiere de interfaz desea continuar: ")
 
-etiqueta = tk.Label(ventana, text="Hola, Bienvenido(a) al proyecto", bg="blue)
-etiqueta.pack(pady=10)
+if preguntar_al_usuario == "si" or preguntar_al_usuario == "sí" or preguntar_al_usuario == "si " or preguntar_al_usuario == "sí ":
+    ventana = tk.Tk()
+    ventana.title("Proyecto GUI")
+    ventana.geometry("400x400")
 
-boton = tk.Button(ventana, text="Saludos", command=saludar_al_usuario)
-boton.pack(pady=6)
+    etiqueta = tk.Label(ventana, text="Hola, Bienvenido(a) al proyecto", bg="blue)
+    etiqueta.pack(pady=10)
+
+    boton = tk.Button(ventana, text="Saludos", command=saludar_al_usuario)
+    boton.pack(pady=6)
                     
-menu_bar = Menu(ventana)
+    menu_bar = Menu(ventana)
                     
-file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Salir", command=salir_pregunta)
-menu_bar.add_cascade(label="Opción", menu=file_menu)
+    file_menu = Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label="Salir", command=salir_pregunta)
+    menu_bar.add_cascade(label="Opción", menu=file_menu)
                     
-ventana.config(menu_bar)
-ventana.mainloop()
+    ventana.config(menu_bar)
+    ventana.mainloop()
+elif preguntar_al_usuario == "no" or preguntar_al_usuario == "no ":
+    print("El programa requiere de interfaz gráfica y si no estás en un entorno gráfico el programa se cierra")
+    sleep(1)
+    exit()
+    
