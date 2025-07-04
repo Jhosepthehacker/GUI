e2c6acef309d3a8daffb1f14378db6fd41b9dc77
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-07-04 19:11:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import Menu
 
+def salir_pregunta():
+    mensaje_de_pregunta = messagebox.askquestion(
+   title="Pregunta",
+   message="¿Quiéres salir de la ventana?"   
+)
+    if (mensaje_de_pregunta == "yes"):
+        print("Ventana cerrada")
+        ventana.destroy()
+    else:
+        print("La ventana todavía sigue abierta")
+      
 ventana = tk.Tk()
 ventana.title("Proyecto GUI")
 ventana.geometry("400x400")
@@ -9,4 +20,8 @@ ventana.geometry("400x400")
 etiqueta = tk.Label(ventana, text="Hola, Bienvenido(a) al proyecto", bg="blue)
 etiqueta.pack(pady=10)
 
+try:
+    ventana.config(menu_bar) # En desarrollo hasta que se creé la barra de menú y opciones
+except NameError:
+    print("Se capturó un NameError")
 ventana.mainloop()
